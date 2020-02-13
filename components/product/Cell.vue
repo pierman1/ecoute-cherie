f3f70e75ce2c448419e4474ee5ce07511151c14e
pierman1 <pierre@lamalama.nl>
2020-02-13 17:42:16 +0100

    wip

---
+++ components/product/Cell.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="product-cell">
+    {{ data.node.title }}
+    <!-- {{ data.node.images }} -->
+    <!-- {{ data.node.priceRange }} -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Cell',
+  props: {
+    data: {
+      type: [Object, Array]
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.product-cell {
+  color: #000;
+  margin-bottom: 10px;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -5,9 +5,24 @@
     </section>
     <section>
       <div class="mx-auto px-6 py-6">
-        <h1 class="font-bold text-xl mb-4">Collections</h1>
+        <h1 class="font-bold text-xl mb-4 text-center">Collections</h1>
         <div class="flex flex-row flex-wrap -mx-2">
           <article
+            class="w-full sm:w-1/2 md:w-1/4 mb-4 px-2"
+          >
+
+            <div class="flex flex-row">
+
+              <cell
+                v-for="(product, index) in collection.products.edges"
+                :key="`product_${index}`"
+                :data="product"
+              />
+
+            </div>
+
+          </article>
+          <!-- <article
             v-for="(collection, index) in collections"
             :key="index"
             @key="collection.node.handle"
@@ -17,9 +32,13 @@
               :to="`/collections/${collection.node.handle}`"
               class="block relative bg-white rounded border p-4"
             >
-              {{ collection.node.title }}
+              {{ collection.title }}
             </nuxt-link>
-          </article>
+
+            <pre>
+              {{ collection.products }}
+            </pre>
+          </article> -->
         </div>
       </div>
     </section>
@@ -28,31 +47,75 @@
 
 <script>
 import Hero from '@/components/hero'
+import Cell from '@/components/product/Cell'
 
 export default {
   components: {
-    Hero
+    Hero,
+    Cell
   },
   data () {
     return {
-      collections: []
+      collection: []
     }
   },
-  asyncData ({ $axios }) {
+  // asyncData ({ $axios }) {
+  //   return $axios.$post(`https://pierttt.myshopify.com/api/graphql`, {
+  //       query: `{
+  //         collections(first:250) {
+  //           edges {
+  //             node {
+  //               handle
+  //               title
+  //               id
+  //             }
+  //           }
+  //         }
+  //       }`
+  //     })
+  //     .then((response) => {
+  //       return { collections: response.data.collections.edges }
+  //     })
+  // }
+  asyncData ({ $axios, params }) {
     return $axios.$post(`https://pierttt.myshopify.com/api/graphql`, {
         query: `{
-          collections(first:250) {
-            edges {
-              node {
-                handle,
-                title
+          collectionByHandle(handle: "frontpage") {
+            id,
+            title,
+            products(first: 250) {
+              edges {
+                node {
+                  handle
+                  title
+                  vendor
+                  images(first: 10) {
+                    edges {
+                      node {
+                        id
+                        src
+                        altText
+                      }
+                    }
+                  }
+                  priceRange {
+                    minVariantPrice {
+                      amount
+                      currencyCode
+                    }
+                    maxVariantPrice {
+                      amount
+                      currencyCode
+                    }
+                  }
+                }
               }
             }
           }
         }`
       })
       .then((response) => {
-        return { collections: response.data.collections.edges }
+        return { collection: response.data.collectionByHandle }
       })
   }
 }
