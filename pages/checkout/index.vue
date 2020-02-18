c35368297c89453f40ccd0e3fb5d2645451072f6
pierman1 <pbleeker@live.nl>
2020-02-19 00:02:18 +0100

    try

---
+++ pages/checkout/index.vue
@@ -0,0 +1,14 @@
+<template lang="html">
+  <section>
+    <iframe src="https://ecoute-cherie.myshopify.com/33104887943/checkouts/b86d7c37582372a936be7ea2dabd64a4?previous_step=shipping_method&step=payment_methods" height="310px" width="99%"></iframe>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'Checkout'
+}
+</script>
+
+<style lang="css">
+</style>
