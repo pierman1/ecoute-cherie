8f8b7c30bc37fc01bbc27aa503ae3ebe75ffe00c
pierman1 <pbleeker@live.nl>
2020-02-18 23:57:07 +0100

    wip

--- service/cartService.js
+++ service/cartService.js
@@ -1,45 +1,41 @@
-
+import axios from 'axios'
 const API_URL = 'https://ecoute-cherie.myshopify.com/api/graphql'
+
+
 class CartService {
 
-  updateCartItem ($axios, checkoutId, variantId, quantity) {
-    console.log('q', quantity)
-    return $axios.$post(API_URL, {
-      query: `mutation {
-        checkoutLineItemsAdd(lineItems: [{ variantId: "${variantId}", quantity: ${quantity} }], checkoutId: "${checkoutId}") {
-          checkout {
-            webUrl
-            subtotalPrice
-            totalTax
-            totalPrice
-            lineItems (first:250) {
-              pageInfo {
-                hasNextPage
-                hasPreviousPage
-              }
-              edges {
-                node {
-                  id
-                  title
-                  variant {
-                    id
-                    title
-                    image {
-                      src
-                    }
-                    price
-                  }
-                  quantity
-                }
-              }
-            }
-          }
-        }
-      }`})
-    }
+  constructor() {
+    let service = axios.create({
+      headers: {
+        'X-Shopify-Storefront-Access-Token': '39595aa67bff7dd0c791ecd8bb438f02',
+        'Content-Type':'application/json'
+      }
+    })
+    service.interceptors.response.use(this.handleSuccess, this.handleError)
+    this.service = service;
+  }
 
-  getCart ($axios, checkoutId) {
-    return $axios.$post('https://ecoute-cherie.myshopify.com/api/graphql', {
+  handleSuccess(response) {
+    return response
+  }
+
+  handleError = (error) => {
+    switch (error.response.status) {
+      case 401:
+        this.redirectTo(document, '/')
+        break;
+      case 404:
+        this.redirectTo(document, '/404')
+        break;
+      default:
+        this.redirectTo(document, '/500')
+        break;
+    }
+    return Promise.reject(error)
+  }
+
+  getCart (checkoutId) {
+    return this.service.post('https://ecoute-cherie.myshopify.com/api/graphql', {
       query: `{
         node(id: "${checkoutId}") {
           ... on Checkout {
@@ -72,6 +68,80 @@ class CartService {
         }
       }`
     })
+  }
+
+  updateCartItem (checkoutId, variantId, quantity) {
+    return this.service.post(API_URL, {
+      query: `mutation {
+      checkoutLineItemsAdd(lineItems: [{ variantId: "${variantId}", quantity: ${quantity} }], checkoutId: "${checkoutId}") {
+        checkout {
+          webUrl
+          subtotalPrice
+          totalTax
+          totalPrice
+          lineItems (first:250) {
+            pageInfo {
+              hasNextPage
+              hasPreviousPage
+            }
+            edges {
+              node {
+                id
+                title
+                variant {
+                  id
+                  title
+                  image {
+                    src
+                  }
+                  price
+                }
+                quantity
+              }
+            }
+          }
+        }
+      }
+    }`})
+  }
+
+  removeCartItem (checkoutId, lineItemId) {
+    return this.service.post('https://ecoute-cherie.myshopify.com/api/graphql', {
+      query: `mutation {
+        checkoutLineItemsRemove(lineItemIds: ["${lineItemId}"], checkoutId: "${checkoutId}") {,
+          userErrors {
+            message
+            field
+          }
+          checkout {
+            webUrl
+            subtotalPrice
+            totalTax
+            totalPrice
+            lineItems (first:250) {
+              pageInfo {
+                hasNextPage
+                hasPreviousPage
+              }
+              edges {
+                node {
+                  id
+                  title
+                  variant {
+                    id
+                    title
+                    image {
+                      src
+                    }
+                    price
+                  }
+                  quantity
+                }
+              }
+            }
+          }
+        }
+      }`})
   }
 
 }
